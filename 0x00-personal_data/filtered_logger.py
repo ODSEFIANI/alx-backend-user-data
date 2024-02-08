20f66a309565890e5e66f9f3ced29aa8866e77f4
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-08 13:41:37 +0100

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,12 @@
 import re
 from typing import List
 
+
 def filter_datum(
         fields: List[str], redaction: str, message: str, separator: str,
         ) -> str:
     """ blure and obscure sensitive data
     """
-    return re.sub(r'(?<=^|{0})({1}=)[^;]*'.format(separator, '|'.join(fields)), r'\1' + redaction, message)
+    return re.sub(r'(?<=^|{0})({1}=)[^;]*'.format(
+        separator, '|'.join(fields)), r'\1' +
+                  redaction, message)
