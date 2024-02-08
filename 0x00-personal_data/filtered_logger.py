96dea4e62e2acce8407733f0355fbac1f72d60ee
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-08 13:39:03 +0100

    0. Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""A module for filtering logs.
+"""
+import re
+from typing import List
+
+def filter_datum(
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """ blure and obscure sensitive data
+    """
+    return re.sub(r'(?<=^|{0})({1}=)[^;]*'.format(separator, '|'.join(fields)), r'\1' + redaction, message)
