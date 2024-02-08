b678545a2c14668efa638f4d55e4d4eba3254602
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-08 13:51:28 +0100

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,10 +6,8 @@ from typing import List
 
 
 def filter_datum(
-        fields: List[str], redaction: str, message: str, separator: str,
+        fields: List[str], redaction: str, message: str, separator: str
         ) -> str:
-    """ blure and obscure sensitive data
-    """
-    return re.sub(r'(?<=^|{0})({1}=)[^;]*'.format(
-        separator, '|'.join(fields)), r'\1' +
-                  redaction, message)
+    """Blur and obscure sensitive data in log messages."""
+    return re.sub(r'(?:(?<=^)|(?<={0}))({1}=)[^;]*'.format(
+        separator, '|'.join(fields)), r'\1' + redaction, message)
