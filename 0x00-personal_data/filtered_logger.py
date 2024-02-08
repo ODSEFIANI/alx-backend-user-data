1ba6df28e4061ddde052e90b45ece9ebfce77705
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-08 15:30:42 +0100

    3. Connect to secure database

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 """A module for filtering logs.
 """
-import re
-from typing import List
 import logging
+import os
+import mysql.connector
+from typing import List
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class"""
 
     REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    FORMAT = "[HOLBERTON] user_data INFO %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
@@ -18,15 +20,65 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
-
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str
-                 ) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """Blur and obscure sensitive data in log messages."""
-    pattern = r'(?:(?<=^)|(?<={0}))({1}=)[^;]*'.format(
-        separator, '|'.join(fields))
+    pattern = r'(?:(?<=^)|(?<={0}))({1}=)[^;]*'.format(separator, '|'.join(fields))
     return re.sub(pattern, r'\1' + redaction, message)
+
+def get_logger() -> logging.Logger:
+    """Return a logger named 'user_data'."""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+
+    # Create a StreamHandler with RedactingFormatter as formatter
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    # Add the StreamHandler to the logger
+    logger.addHandler(stream_handler)
+
+    # Prevent messages from being propagated to other loggers
+    logger.propagate = False
+
+    return logger
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """Return a connector to the database."""
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database_name = os.getenv("PERSONAL_DATA_DB_NAME", "")
+
+    db = mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database_name
+    )
+
+    return db
+
+def main():
+    """Retrieve all rows in the users table and display each row under a filtered format."""
+    logger = get_logger()
+
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+
+    logger.info("Filtered fields:\n%s", '\n'.join(PII_FIELDS))
+
+    for row in cursor:
+        log_message = "; ".join(f"{field}={row[i]}" for i, field in enumerate(PII_FIELDS))
+        logger.info(log_message)
+
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
