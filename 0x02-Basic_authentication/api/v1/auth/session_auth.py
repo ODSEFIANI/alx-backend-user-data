788f3b156cbe0e9fbedbc2b51875ca5dc99ebddb
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-17 01:02:17 +0100

    2. Create a session

---
+++ 0x02-Basic_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+Session clss module
+"""
+import base64
+from typing import TypeVar
+from .auth import Auth
+from models.user import User
+from uuid import uuid4
+
+
+class SessionAuth(Auth):
+    """Session authentication class
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+            return None
+        if not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+
+        # Placeholder: You might want to return the generated session_id
+        return self.user_id_by_session_id
+
+
+
+
+        
