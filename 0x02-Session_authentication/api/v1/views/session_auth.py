6c293b396098d9bf85cb8a74cddec96ff63c6bcd
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-18 02:53:02 +0100

    7. New view for Session Authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, abort, make_response
 from api.v1.views import app_views
 from models.user import User
 from api.v1.app import auth
+import os
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
 def session_login():
@@ -23,26 +24,25 @@ def session_login():
 
     # Retrieve the User instance based on the email
     users = User.search({"email": email})
-    if not users:
+    if not users or users == []:
         return jsonify({"error": "no user found for this email"}), 404
 
-    user = users[0]
+    for user in users:
+        if user.is_valid_password(password):
+            # Create a Session ID for the User ID
+            session_id = auth.create_session(user.id)
 
-    # Check if the password is correct
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
+            # Return the dictionary representation of the User
+            user_dict = user.to_json()
 
-    # Create a Session ID for the User ID
-    session_id = auth.create_session(user.id)
+            # Set the cookie to the response
+            session_cookie_name = os.getenv('SESSION_NAME')
+            response = make_response(jsonify(user_dict))
+            response.set_cookie(session_cookie_name, session_id)
 
-    # Return the dictionary representation of the User
-    user_dict = user.to_json()
+            return response
 
-    # Set the cookie to the response
-    session_cookie_name = app.config.get('SESSION_NAME')
-    response = make_response(jsonify(user_dict))
-    response.set_cookie(session_cookie_name, session_id)
-
-    return response
+    # If no user with matching credentials is found
+    return jsonify({"error": "wrong password"}), 401
 
 
