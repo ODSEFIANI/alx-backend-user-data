10481d81f2238e3d9ca0488f596023cfe5b375fa
ODSEFIANI <oussma.sefiani@um5r.ac.ma>
2024-02-23 22:58:59 +0100

    8. Credentials validation

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -14,7 +14,7 @@ def home():
 
 
 @app.route("/users", methods=["POST"])
-def create_user():
+def generate_user():
     """generate a new user"""
     email = request.form.get("email")
     password = request.form.get("password")
@@ -23,3 +23,16 @@ def create_user():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": "{}".format(email), "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """generates a user_session """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    responce = jsonify({"email": email, "message": "logged in"})
+    session_id = AUTH.create_session(email)
+    responce.set_cookie("session_id", session_id)
+    return responce
